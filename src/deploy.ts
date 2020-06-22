221417278713356073e2b4c5aa7314e036e3a2ec
Luca Sbardella <luca@quantmind.com>
2020-06-22 10:36:33 +0100

    Fix action.yml

--- src/deploy.ts
+++ src/deploy.ts
@@ -3,8 +3,10 @@ import github from "@actions/github";
 
 const deploy = () => {
   const { context } = github;
-  const metablockToken = core.getInput("metablock-token", { required: true });
-  const metablockId = core.getInput("metablock-block-id");
+  const metablockToken = core.getInput("metablock-api-token", { required: true });
+  const metablockId = core.getInput("metablock-block-id", { required: true });
+  const metablockEnv = core.getInput("metablock-env") || "stage";
+  const bundle = core.getInput("metablock-block-id", { required: true });
 };
 
 export default deploy;
